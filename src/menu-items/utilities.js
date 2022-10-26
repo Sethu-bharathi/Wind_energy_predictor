abfd5e83d0b29b7796058aa47ba28fa7f29bc673
Sethu-bharathi <bharathisethu018@gmail.com>
2022-10-26 19:50:08 +0530

    First commit

---
+++ src/menu-items/utilities.js
@@ -0,0 +1,53 @@
+// assets
+import {
+    AppstoreAddOutlined,
+    AntDesignOutlined,
+    BarcodeOutlined,
+    BgColorsOutlined,
+    FontSizeOutlined,
+    LoadingOutlined
+} from '@ant-design/icons';
+
+// icons
+const icons = {
+    FontSizeOutlined,
+    BgColorsOutlined,
+    BarcodeOutlined,
+    AntDesignOutlined,
+    LoadingOutlined,
+    AppstoreAddOutlined
+};
+
+// ==============================|| MENU ITEMS - UTILITIES ||============================== //
+// ======[Navbar elements]===== //
+const utilities = {
+    id: 'utilities',
+    title: 'Utilities',
+    type: 'group',
+    children: [
+        {
+            id: 'util-location',
+            title: 'Predict Location',
+            type: 'item',
+            url: '/shadow',
+            icon: icons.BarcodeOutlined
+        },
+        {
+            id: 'predict-manual',
+            title: 'Predict Manual',
+            type: 'item',
+            url: '/predictManual',
+            icon: icons.AntDesignOutlined,
+            breadcrumbs: false
+        },
+        {
+            id: 'util-compare-location',
+            title: 'Compare Locations',
+            type: 'item',
+            url: '/color',
+            icon: icons.BgColorsOutlined
+        }
+    ]
+};
+
+export default utilities;
---
+++ src/routes/MainRoutes.js
@@ -0,0 +1,61 @@
+import { lazy } from 'react';
+
+// project import
+import Loadable from 'components/Loadable';
+import MainLayout from 'layout/MainLayout';
+
+// render - dashboard
+const Dashboard = Loadable(lazy(() => import('pages/dashboard')));
+
+// render - sample page
+const SamplePage = Loadable(lazy(() => import('pages/extra-pages/SamplePage')));
+
+// render - utilities
+const PredictManual = Loadable(lazy(() => import('pages/components-overview/PredictManual')));
+const Typography = Loadable(lazy(() => import('pages/components-overview/Typography')));
+const Color = Loadable(lazy(() => import('pages/components-overview/Color')));
+const Shadow = Loadable(lazy(() => import('pages/components-overview/Shadow')));
+const AntIcons = Loadable(lazy(() => import('pages/components-overview/AntIcons')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+        {
+            path: '/',
+            element: <Dashboard />
+        },
+        {
+            path: 'color',
+            element: <Color />
+        },
+        {
+            path: 'dashboard',
+            element: <Dashboard />
+        },
+        {
+            path: 'sample-page',
+            element: <SamplePage />
+        },
+        {
+            path: 'shadow',
+            element: <Shadow />
+        },
+        {
+            path: 'typography',
+            element: <Typography />
+        },
+        {
+            path: 'PredictManual',
+            element: <PredictManual />
+        },
+        {
+            path: 'icons/ant',
+            element: <AntIcons />
+        }
+    ]
+};
+
+export default MainRoutes;
